5ea88e76bbec933db005c38055a6ced9e03d3d3d
jmwebb <jmwebb.personal@gmail.com>
2023-08-30 10:04:38 -0700

    Worked out LH and RH addition for Value struct and f32

--- microgrust/src/main.rs
+++ microgrust/src/main.rs
@@ -5,18 +5,48 @@ use std::fmt;
 use std::ops;
 
 struct Value {
-    data: i32,
-    // _prev: HashSet<Value>,
+    data: f32,
+    _prev: Vec<Value>,
 }
 
+impl Value {
+    fn new(data: f32) -> Self {
+        Value {
+            data,
+            _prev: Vec::new(),
+        }
+    }
+    
+}
+
+// ╔══════════════════════════╗
+// ║        Addition          ║
+// ╚══════════════════════════╝
 impl ops::Add<Value> for Value {
     type Output = Value;
 
     fn add(self, rhs: Value) -> Value {
         Value {
             data: self.data + rhs.data,
-            // _prev: Some(Box::new(self)),
+            _prev: vec![self, rhs],
         }
+    }
+}
+
+impl ops::Add<f32> for Value {
+    type Output = Value;
+
+    fn add(self, rhs: f32) -> Value {
+        let rhs = Value::new(rhs);
+        self + rhs
+    }
+}
+
+impl ops::Add<Value> for f32 {
+    type Output = Value;
+
+    fn add(self, rhs: Value) -> Value {
+        rhs + self
     }
 }
 
@@ -33,8 +63,9 @@ impl fmt::Display for Value {
 }
 
 fn main() {
-    let a = Value {data: 1};
-    let b = Value {data: 2};
+    let b = Value::new(1.0);
+    let a = 2.0;
     let c = a + b;
     println!("c = {:?}", c);
+    println!("c = {:?}", c._prev);
 }
